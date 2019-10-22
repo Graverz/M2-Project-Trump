012c9e8fcaf9afa25f2ea22e849033453f30fa61
Jess Holstein Nielsen <47520884+holle94@users.noreply.github.com>
2019-10-22 15:33:20 +0200

    Add files via upload

--- Trump_data.R
+++ Trump_data.R
@@ -1,7 +1,13 @@
-library(tidyverse)
-library(RJSONIO)
-library(lubridate)
 
+if (!require("pacman")) install.packages("pacman") # package for loading and checking packages :)
+pacman::p_load(tidyverse, # Standard datasciewnce toolkid (dplyr, ggplot2 et al.)
+               magrittr, # For advanced piping (%>% et al.)
+               tidytext, # For text analysis
+               tm,
+               topicmodels,
+               RJSONIO,
+               lubridate
+)
 
 json_file17 <- fromJSON("//student.aau.dk/Users/jhhn14/Desktop/condensed_2017.json")
 json_file18 <- fromJSON("//student.aau.dk/Users/jhhn14/Desktop/condensed_2018.json")
@@ -21,6 +27,7 @@ data18 <- do.call("rbind", json_file18)
 data <- rbind(data17,data18) %>% as.data.frame()
 
 data$created_at <- data$created_at %>% as.character.Date()
+data$text <- data$text %>%  as.character()
 
 str(data)
 data$day  <- str_sub(data$created_at,5, 10)
@@ -28,6 +35,9 @@ data$year <- str_sub(data$created_at,27)
 #data$time <- str_sub(data$created_at, 12,19)
 data$date <- str_c(data$day,data$year)  %>% mdy
 
-trump_tweet <- data[,-c(1,2,4,9,10)]
+trump_tweet <- data[,c("id_str","text","date")]
+
+
+write.csv(trump_tweet, "Trump_tweets.csv")
 
 
